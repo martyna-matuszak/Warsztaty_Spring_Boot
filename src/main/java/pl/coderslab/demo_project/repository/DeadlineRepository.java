bf77db0f835a70d8b3723286129b062c5052ac49
Martyna <martyna.matuszak96@gmail.com>
2020-07-02 18:34:52 +0200

    updated DeadlineRepository - new function

--- src/main/java/pl/coderslab/demo_project/repository/DeadlineRepository.java
+++ src/main/java/pl/coderslab/demo_project/repository/DeadlineRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.coderslab.demo_project.entity.Clinic;
 import pl.coderslab.demo_project.entity.Deadline;
 import pl.coderslab.demo_project.entity.Doctor;
+import pl.coderslab.demo_project.entity.Patient;
 
 import java.util.List;
 
 public interface DeadlineRepository extends JpaRepository<Deadline, Long> {
 
     List<Deadline> findAllByPatientIdOrderByDueDate(Long id);
+    List<Deadline> findAllByPatientId(Long id);
 }
